819c5b17fb6cf78fa6e5000784d3735b0806fced
AmyangXYZ <amyang.xyz@gmail.com>
2024-04-23 00:28:08 -0400

    fix outline and changing to rapier3D physics

--- src/hooks/useCharModel.ts
+++ src/hooks/useCharModel.ts
@@ -4,26 +4,40 @@ import * as THREE from 'three'
 import { MMDLoader } from 'three/examples/jsm/loaders/MMDLoader'
 import { MMDAnimationHelper } from 'three/examples/jsm/animation/MMDAnimationHelper'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
-import { BrightnessContrastShader } from 'three/examples/jsm/shaders/BrightnessContrastShader'
-import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
-import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass'
-
+import { OutlineEffect } from 'three/examples/jsm/effects/OutlineEffect.js'
 import Stats from 'three/addons/libs/stats.module.js'
+import * as RAPIER from '@dimforge/rapier3d'
 
 export function useCharModel(container: HTMLElement) {
-  let composer: any
+  let renderer: any, camera: any
   const stats = new Stats()
   const clock = new THREE.Clock()
   const animationHelper = new MMDAnimationHelper({ afterglow: 2.0 })
   let ikHelper: any, physicsHelper: any
   const loader = new MMDLoader()
   let model: THREE.Object3D | undefined = undefined
-  let outlinePass: any
-
+  let effect: OutlineEffect
   const scene = new THREE.Scene()
+
+  const gravity = { x: 0.0, y: -1, z: 0.0 }
+  const world = new RAPIER.World(gravity)
+
+  // Create the ground
+  const groundGeometry = new THREE.PlaneGeometry(10, 10)
+  const groundMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc })
+  const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial)
+  groundMesh.rotation.x = -Math.PI / 2
+  groundMesh.position.y = -10
+  scene.add(groundMesh)
+
+  const groundBodyDesc = RAPIER.RigidBodyDesc.fixed().setTranslation(
+    groundMesh.position.x,
+    groundMesh.position.y,
+    groundMesh.position.z
+  )
+  const groundBody = world.createRigidBody(groundBodyDesc)
+  const groundColliderDesc = RAPIER.ColliderDesc.cuboid(5, 0.1, 5)
+  world.createCollider(groundColliderDesc, groundBody)
 
   const LoadChar = (char: string) => {
     if (model != undefined) {
@@ -39,11 +53,11 @@ export function useCharModel(container: HTMLElement) {
     const vmdFiles = ['/motions/1.vmd']
 
     if (LoadedModels[char] == undefined) {
-      loader.loadWithAnimation(mmdFile, vmdFiles, (mmd: any) => {
-        const mesh = mmd.mesh
+      loader.loadWithAnimation(mmdFile, vmdFiles, (m: any) => {
+        const mesh = m.mesh
         mesh.castShadow = true // Enable casting shadows
         mesh.receiveShadow = true // Enable receiving shadows
-        mesh.position.y = -15
+        mesh.position.y = -5
         model = mesh
         LoadedModels[char] = model
 
@@ -51,21 +65,28 @@ export function useCharModel(container: HTMLElement) {
         scene.add(model!)
 
         animationHelper.add(model, {
-          animation: mmd.animation,
-          physics: true,
-          gravity: new THREE.Vector3(0, -6, 0),
-          maxStepNum: 3
+          // animation: mmd.animation,
+          physics: false // disable Ammojs-based physics
         })
 
-        // animationHelper.enable('animation', false)
+        // rapier3D based physics
+        const mmd = m.mesh.geometry.userData.MMD
+        for (const rb of mmd.rigidBodies) {
+          console.log(rb)
+          const rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
+            .setTranslation(rb.position[0], rb.position[1], rb.position[2])
+            .setRotation({
+              w: 1,
+              x: rb.rotation[0],
+              y: rb.rotation[1],
+              z: rb.rotation[2]
+            })
+          const rigidBody = world.createRigidBody(rigidBodyDesc)
+          const colliderDesc = RAPIER.ColliderDesc.ball(rb.width)
+          world.createCollider(colliderDesc, rigidBody)
 
-        ikHelper = animationHelper.objects.get(mesh).ikSolver.createHelper()
-        ikHelper.visible = false
-        scene.add(ikHelper)
-
-        physicsHelper = animationHelper.objects.get(mesh).physics.createHelper()
-        physicsHelper.visible = true
-        scene.add(physicsHelper)
+          // rb.userData.rigidBody = rigidBody
+        }
       })
     } else {
       model = LoadedModels[char]
@@ -79,31 +100,25 @@ export function useCharModel(container: HTMLElement) {
     })
   }
 
-  const init = () => {
-    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+  const initScene = () => {
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
     renderer.setPixelRatio(window.devicePixelRatio * 2)
     renderer.setSize(container.clientWidth, container.clientHeight)
     container.appendChild(renderer.domElement)
-    renderer.outputColorSpace = THREE.SRGBColorSpace
     renderer.shadowMap.enabled = true
-    renderer.toneMapping = THREE.ReinhardToneMapping
-    renderer.toneMappingExposure = 1
 
-    const camera = new THREE.PerspectiveCamera(
-      50,
-      container.clientWidth / container.clientHeight,
-      1,
-      100
-    )
+    effect = new OutlineEffect(renderer)
+
+    camera = new THREE.PerspectiveCamera(50, container.clientWidth / container.clientHeight, 1, 100)
     camera.position.set(-2, 3, 15)
     camera.lookAt(new THREE.Vector3(0, 0, 0))
 
     // Ambient light
-    const ambientLight = new THREE.AmbientLight(0xffeeff, 2)
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1)
     scene.add(ambientLight)
 
     // Directional light
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1.4)
     directionalLight.position.set(-2, 7, 18)
     directionalLight.castShadow = true
     directionalLight.shadow.bias = -0.001
@@ -111,6 +126,14 @@ export function useCharModel(container: HTMLElement) {
     directionalLight.shadow.mapSize.height = 2048
     scene.add(directionalLight)
 
+    stats.dom.style.position = 'fixed'
+    stats.dom.style.left = '16px'
+    stats.dom.style.bottom = '16px'
+    stats.dom.style.top = ''
+    container.appendChild(stats.dom)
+  }
+
+  const initEventHandlers = () => {
     const controls = new OrbitControls(camera, renderer.domElement)
     controls.mouseButtons = {
       LEFT: undefined,
@@ -119,7 +142,6 @@ export function useCharModel(container: HTMLElement) {
 
     let isDragging = false
     let previousMousePosition = { x: 0, y: 0 }
-
     window.addEventListener('mousedown', (event: MouseEvent) => {
       if (event.button == 0) {
         isDragging = true
@@ -149,70 +171,43 @@ export function useCharModel(container: HTMLElement) {
       renderer.setSize(container.clientWidth, container.clientHeight)
     }
     window.addEventListener('resize', resizeHandler)
-
-    composer = new EffectComposer(renderer)
-    const renderPass = new RenderPass(scene, camera)
-    composer.addPass(renderPass)
-
-    outlinePass = new OutlinePass(
-      new THREE.Vector2(container.clientWidth, container.clientHeight),
-      scene,
-      camera
-    )
-    outlinePass.edgeStrength = 2
-    outlinePass.edgeGlow = 1
-    outlinePass.edgeThickness = 1
-    outlinePass.pulsePeriod = 0
-    outlinePass.usePatternTexture = false // patter texture for an object mesh
-    outlinePass.visibleEdgeColor.set('#000000') // set basic edge color
-    outlinePass.hiddenEdgeColor.set('#000000') // set edge color when it hidden by other objects
-    composer.addPass(outlinePass)
-
-    const bloomPass = new UnrealBloomPass(
-      new THREE.Vector2(window.innerWidth, window.innerHeight),
-      1,
-      1,
-      1
-    )
-    bloomPass.threshold = 0.2 // Adjust the luminance threshold for bloom
-    bloomPass.strength = 0.06 // Increase the bloom strength
-    bloomPass.radius = 25 // Increase the bloom radius
-    composer.addPass(bloomPass)
-
-    const brightnessContrastPass = new ShaderPass(BrightnessContrastShader)
-    brightnessContrastPass.uniforms.brightness.value = 0 // Adjust brightness
-    brightnessContrastPass.uniforms.contrast.value = 0 // Adjust contrast
-    composer.addPass(brightnessContrastPass)
-
-    stats.dom.style.position = 'fixed'
-    stats.dom.style.left = '16px'
-    stats.dom.style.bottom = '16px'
-    stats.dom.style.top = ''
-    container.appendChild(stats.dom)
   }
+
+  // rapier3D debug lines
+  const lineGeometry = new THREE.BufferGeometry()
+  const lineMaterial = new THREE.LineBasicMaterial({ vertexColors: false })
+  const lineSegments = new THREE.LineSegments(lineGeometry, lineMaterial)
+  scene.add(lineSegments)
 
   const animate = () => {
     requestAnimationFrame(animate)
     stats.begin()
-    composer.render()
+
+    // Get the debug render data from the physics world
+    const { vertices, colors } = world.debugRender()
+    lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
+    lineGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 4))
+    lineGeometry.attributes.position.needsUpdate = true
+
+    world.step()
+    effect.render(scene, camera)
     animationHelper.update(clock.getDelta())
 
-    if (model != undefined) {
-      outlinePass.selectedObjects = [model]
-    }
+    scene.traverse(function (object) {
+      if (object.userData.rigidBody) {
+        object.position.copy(object.userData.rigidBody.translation())
+        object.quaternion.copy(object.userData.rigidBody.rotation())
+      }
+    })
+
     stats.end()
   }
 
   const main = () => {
-    if (typeof Ammo === 'function') {
-      Ammo().then(function (AmmoLib: any) {
-        Ammo = AmmoLib
-      })
-    }
-    init()
+    initScene()
+    initEventHandlers()
     animate()
   }
   main()
-
   return { LoadChar, LoadPose }
 }
