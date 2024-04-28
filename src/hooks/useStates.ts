a9fc319256bf76e48f3c2dcd0e64a78dcba06f27
AmyangXYZ <amyang.xyz@gmail.com>
2024-04-28 17:52:19 -0400

    rename motions

--- src/hooks/useStates.ts
+++ src/hooks/useStates.ts
@@ -14,6 +14,6 @@ export const Motions = ['Miku', 'Zyy', 'Stand', 'iKun1', 'iKun2']
 
 export const FPS = ref(0)
 export const SelectedChar = ref('Thoth')
-export const SelectedAnimation = ref('4')
+export const SelectedAnimation = ref('Stand')
 export const ShowRigidBodies = ref(false)
 export const ShowFPS = ref(true)
